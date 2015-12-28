e2fec9f6806feb5121e5b76a0867b4a0923fa49f
Hajime Kawahara <divrot@gmail.com>
2015-12-28 10:38:50 +0900

    add extif

--- extif.jl
+++ extif.jl
@@ -12,4 +12,17 @@ function maxif(tfr)
     return indf
 end
 
+function aveif(tfr)
+    nf,nt=size(tfr)
+    indf=zeros(nt)
+    ifreq=1:nf
+    for it=1:nt
+        i=sum(ifreq[:].*tfr[:,it])/sum(tfr[:,it])
+        indf[it]=i
+    end 
+    
+    return indf
+end
+
+
 end
