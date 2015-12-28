9200bf48389061543d8c8047715a5bd1a4e5dc1e
Hajime Kawahara <divrot@gmail.com>
2015-12-28 22:47:23 +0900

    temporary

--- estif.jl
+++ estif.jl
@@ -2,6 +2,7 @@ module estif
 #import DSP
 import cohenclass
 import extif
+import polywv
 
 function ifestxvwd(z,ynorm,dx,niter)
     tfrn=cohenclass.tfrwv(z,NaN,NaN,1)
@@ -27,5 +28,17 @@ function ifestxpvwd(z,ynorm,dx,niter)
     return indfn
 end
 
+function ifestxp4vwd(z,ynorm,dx,niter)
+
+    tfrn=polywv.tfr4powv(z,NaN,NaN,1)
+    indfn=extif.maxif(tfrn)
+    for it=1:niter        
+        zhat=exp(-im*cumsum(indfn)*ynorm*dx)'
+        zc=[z;zhat]
+        tfrn=polywv.tfr4powv(zc,NaN,NaN,1)
+        indfn=extif.maxif(abs(tfrn))
+    end
+    return indfn
+end
 
 end
--- polywv.jl
+++ polywv.jl
@@ -1,23 +1,22 @@
 module polywv
 
 ##### DO NOT WORKING YET ####
-
-function tfr4powv(x,t=NaN,N=NaN)
+function tfr6powv(x,t=NaN,N=NaN,silent=0)
     println("DON'T USE !!! DO NOT WORKING YET.")
     #fourth order polinomial
     xcol,xrow = size(x) #xcol for cross-rwv
     if isnan(t) t=(1:xrow)' end
     if isnan(N) N=xrow end
     #information
-    println("Sizes of x and t")
-    println(size(x))
-    println(size(t))
+#    println("Sizes of x and t")
+#    println(size(x))
+#    println(size(t))
 
     #error handling
     if N<0; println("N must be greater than zero"); exit(); end
     if xcol==0 || xcol>2; println("X must have one or two columns"); exit() end
-    if xcol==1; println("Single Wigner Ville"); end
-    if xcol==2; println("Cross Wigner Ville"); end
+    if xcol==1 && silent==0; println("Single Wigner Ville"); end
+    if xcol==2 && silent==0; println("Cross Wigner Ville"); end
     if nextpow2(N)!=N; println("For a faster computation, N should be a power of two\n"); end
 
     tfr=zeros(Complex64,N,N) # plane by default
@@ -27,15 +26,16 @@ function tfr4powv(x,t=NaN,N=NaN)
         taumax=minimum([ti-1,xrow-ti,round(N/4)-1])
         tau=round(Int64,-taumax:taumax); indices=round(Int64,rem(N+tau,N)+1)
         tfr[indices,icol] = x[1,ti+tau].*conj(x[xcol,ti-tau]).*x[1,ti+tau].*conj(x[xcol,ti-tau]) #                
-
-        tau=round(N/4); 
-        if ti<=xrow-tau && ti>=tau+1
-            tfr[tau+1,icol] = 0.5*(x[1,ti+tau]^2*conj(x[xcol,ti-tau])^2 + x[1,ti-tau]^2*conj(x[xcol,ti+tau]^2))
-        end
+#        tau=round(N/4); 
+#        if ti<=xrow-tau && ti>=tau+1
+#            tfr[tau+1,icol] = 0.5*(x[1,ti+tau]^2*conj(x[xcol,ti-tau])^2 + x[1,ti-tau]^2*conj(x[xcol,ti+tau]^2))
+#        end
     end
     for i=1:N
         tfr[:,i]=fft(tfr[:,i])
     end
+#    println(tfr)
+#    exit()
     if xcol==1
         tfr=real(tfr)
     end
@@ -45,9 +45,9 @@ end
 
 end
 
-import DSP
-y=[1.,2.,3.,5.,1.,2.,3.,5.,1.,2.,3.,5.,1.,2.,3.,5.]
-ya=DSP.Util.hilbert(y) # transpose is necessary 
-y=conj(ya')
-tfr=tfr4powv(y)
-println(real(tfr))
+#import DSP
+#y=[1.,2.,3.,5.,1.,2.,3.,5.,1.,2.,3.,5.,1.,2.,3.,5.]
+#ya=DSP.Util.hilbert(y) # transpose is necessary 
+#y=conj(ya')
+#tfr=polywv.tfr4powv(y)
+#println(real(tfr))
