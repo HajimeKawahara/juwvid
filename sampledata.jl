e866eba9c913118f23deb526c816803e832e3690
Hajime Kawahara <divrot@gmail.com>
2015-12-28 13:14:14 +0900

    normalization

--- sampledata.jl
+++ sampledata.jl
@@ -1,9 +1,19 @@
 module sampledata
 
-function genfm(nsamp,wp=1.0,wf=0.01,s=1.0)
-    x=linspace(0.0,100pi,nsamp)
+function genfm(nsamp,wp=1.0,wf=0.01,s=1.0, xend=100*pi)
+    x=linspace(0.0,xend,nsamp)
     y=cos(wp*x+s*sin(wf*x))
-    return x, y
+    iw=wp+s*wf*cos(wf*x)
+    ynorm=pi/x[end]
+    return x, y, iw, ynorm
+end
+
+function genlinfm(nsamp,wp=1.0,s=0.01, xend=100*pi)
+    x=linspace(0.0,xend,nsamp)
+    y=cos(wp*x+0.5*s*x.*x)
+    iw=wp+s*x
+    ynorm=pi/x[end]
+    return x, y, iw, ynorm
 end
 
 end
