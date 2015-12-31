a54397e59a60f613df0d5cc3e7f9c17111e60deb
Hajime Kawahara <divrot@gmail.com>
2015-12-31 23:54:31 +0900

    tmp

--- estif.jl
+++ estif.jl
@@ -4,7 +4,7 @@ import cohenclass
 import extif
 import polywv
 
-function ifestxvwd(z,ynorm,dx,niter)
+function ifestxvwd(z,ynorm,dx,niter, tfrsw=false)
     tfrn=cohenclass.tfrwv(z,NaN,NaN,NaN,1)
     indfn=extif.maxif(real(tfrn))
     for it=1:niter        
@@ -12,10 +12,14 @@ function ifestxvwd(z,ynorm,dx,niter)
         tfrn=cohenclass.tfrwv(z,zhat,NaN,NaN,1)
         indfn=extif.maxif(abs(tfrn))
     end
-    return indfn
+    if tfrsw
+        return indfn, tfrn
+    else
+        return indfn
+    end
 end
 
-function ifestxpvwd(z,ynorm,dx,niter)
+function ifestxpvwd(z,ynorm,dx,niter, tfrsw=false)
     tfrn=cohenclass.tfrpwv(z,NaN,NaN,NaN,NaN,1)
     indfn=extif.maxif(real(tfrn))
     for it=1:niter
@@ -23,7 +27,11 @@ function ifestxpvwd(z,ynorm,dx,niter)
         tfrn=cohenclass.tfrpwv(z,zhat,NaN,NaN,NaN,1)
         indfn=extif.maxif(abs(tfrn))
     end
-    return indfn
+    if tfrsw
+        return indfn, tfrn
+    else
+        return indfn
+    end
 end
 
 function ifestxpowv(z,ynorm,dx,niter, tfrsw=false)
