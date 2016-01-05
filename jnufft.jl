e009d687b909fc0c44e0c44847a681dc3efc5a76
Hajime Kawahara <divrot@gmail.com>
2016-01-05 23:23:06 +0900

    rename to jnufft

--- jnufft.jl
+++ jnufft.jl
@@ -1,5 +1,5 @@
 module jnufft
-#simple julia wrapper of fortran 90 subroutine printint in ionufft module 
+#Change the path to libnufft.so in ccall
 
 function call_ionufft1d2(xj,fk,iflag=-1,eps=10.0^-24)
 
@@ -12,7 +12,7 @@ function call_ionufft1d2(xj,fk,iflag=-1,eps=10.0^-24)
 
     xj0 = zeros(Float64,ms)     
     for j = 1:nj
-        xj0[j] = xj[j]
+        xj0[j] = 2*pi*xj[j]/ms
     end
 
     fk0 = zeros(Complex128,ms) 
@@ -22,21 +22,17 @@ function call_ionufft1d2(xj,fk,iflag=-1,eps=10.0^-24)
     for j = round(Int,ms/2+1):ms          
         fk0[j] = fk[round(Int,j-ms/2)]
     end
-
-    product = ccall((:__ionufft_MOD_ionufft1d2, "nufft_src/libnufft.so"),Int32,(Ptr{Int32},Ptr{Int32},Ptr{Int32},Ptr{Float64},Ptr{Float64},Ptr{Complex128},Ptr{Complex128}),&nj,&ms,&iflag,&eps,xj0,fk0,cj)
+    product = ccall((:__ionufft_MOD_ionufft1d2, "/Users/kawahara/juwvid/nufft_src/libnufft.so"),Int32,(Ptr{Int32},Ptr{Int32},Ptr{Int32},Ptr{Float64},Ptr{Float64},Ptr{Complex128},Ptr{Complex128}),&nj,&ms,&iflag,&eps,xj0,fk0,cj)
     #    return cj[1:ms]
     return unshift!(cj[1:ms-1], cj[ms])
 end
 
 end
 
-ms=8
-nj=8 
-xj = zeros(ms)     
-for i = 1:nj
-    xj[i] = 2*pi*real(i)/ms 
-end
-fk = collect(1:ms)
-cj=jnufft.call_ionufft1d2(xj,fk)
-println(cj)
-    
+#ms=8
+#nj=8 
+#xj = collect(1:ms)     
+#fk = collect(1:ms)
+#cj=jnufft.call_ionufft1d2(xj,fk,-1,10.0^-32)
+#println(cj)
+
