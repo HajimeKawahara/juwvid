3ed3bca8a2cc07a6d346b2cd2f381a1b607a1b84
Hajime Kawahara <divrot@gmail.com>
2016-01-06 15:47:35 +0900

    fixed a small bug in estif

--- estif.jl
+++ estif.jl
@@ -39,14 +39,35 @@ function ifestxvwd(z,ynorm,dx,niter,indfn=NaN,isf=NaN,ief=NaN,fin=NaN,tfrsw=fals
     end
 end
 
-function ifestxpvwd(z,ynorm,dx,niter, tfrsw=false)
-    tfrn=cohenclass.tfrpwv(z,NaN,NaN,NaN,NaN,1)
-    indfn=extif.maxif(real(tfrn))
+function ifestxpvwd(z,ynorm,dx,niter,indfn=NaN,isf=NaN,ief=NaN,fin=NaN,tfrsw=false)
+    if isnan(fin[1])
+        nf=size(z)
+    else
+        nf=length(fin)
+    end
+
+    if isnan(isf) isf=1 end 
+    if isnan(ief) ief=nf end 
+
+    if isnan(indfn[1])
+        tfrn=cohenclass.tfrpwv(z,NaN,NaN,NaN,NaN,1)
+        indfn=extif.maxif(real(tfrn))
+    end
+
     for it=1:niter
         zhat=exp(im*cumsum(indfn*ynorm*dx))
-        tfrn=cohenclass.tfrpwv(z,zhat,NaN,NaN,NaN,1)
-        indfn=extif.maxif(abs(tfrn))
+        if isnan(fin[1])
+            tfrn=cohenclass.tfrpwv(z,zhat,NaN,NaN,NaN,1)
+            indfn=extif.maxif(abs(tfrn))
+        else
+            tfrn=cohenclass.tfrpwv(z,zhat,NaN,fin,NaN,NaN,0);
+            indfna=extif.maxif(abs(tfrn),isf,ief)
+            indfn=fin[round(Int,indfna)]
+        end
+
     end
+
+
     if tfrsw
         return indfn, tfrn
     else
