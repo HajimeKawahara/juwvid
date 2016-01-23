ec3fb2860b315994e3eca00db0d993a2666040fe
Hajime Kawahara <divrot@gmail.com>
2016-01-23 13:00:44 +0900

    adaptive algorithm

--- pwvaw.jl
+++ pwvaw.jl
@@ -1,8 +1,11 @@
-module pwvaw #pseudo Wigner distribution with the adaptive 
+module pwvaw #pseudo Wigner distribution with the adaptive algorithm
 import jnufft
 
-function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,use_nufft=true)
-    #method = median : robust Wigner distribution
+function awpwv(x,sigmaeA,y=NaN,t=NaN,f=NaN,N=NaN,kappa=5.0,delkappa=0.97,silent=0,method="mean",use_nufft=true)
+    # adaptive algorithm of pseudo Wigner-Ville distribution 
+    # Reference Time Frequency Signal Analysis with Applications, 2013, Stanlovi and Dakovic p420
+    # sigmaeA[1:length(x)]: sigmae/A
+    # If f is not NaN, use NUFFT by default
 
     wtype="rect"
 #    wtype="Hamming"
@@ -11,11 +14,11 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
     if isnan(t)[1] t=collect(1:xrow) end
     if isnan(N) N=xrow end
     if isnan(y)[1] 
-        if silent ==0 println("Single pseudo Wigner Ville") end
+        if silent ==0 println("Single pseudo Wigner Ville with the Adaptive Algorithm") end
         y=x
         sw=0
     else
-        if silent ==0 println("Cross pseudo Wigner Ville") end
+        if silent ==0 println("Cross pseudo Wigner Ville with the Adaptive Algorithm") end
         sw=1
     end
 
@@ -30,15 +33,14 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
     #set window array
     M=N/2
     harray=[M]
-#    println(harray)
     while M>4
         M=M/2
         push!(harray,M)       
     end
     harray=harray[end:-1:1]
     windows=[]
-#    println("Adaptive window from =",harray)
 
+    #
     for icol=1:N
         crit=true
         prevcrit=false
@@ -46,7 +48,6 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
 
         while crit && ih < length(harray) 
             ih=ih+1
-#            println("ih=",ih)
             tfrvec=zeros(Complex64,N) #
             hlength=floor(harray[ih])
             hlength=hlength+1-rem(hlength,2)        
@@ -54,14 +55,12 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
             if ismatch(r"Hamming",wtype)         
                 h=0.54 - 0.46*cos(2.0*pi*(1:hlength)/(hlength+1)) #Hamming
             else
-#                #rectangular
-                h=ones(round(Int,hlength))
+                h=ones(round(Int,hlength)) #Rectangular
             end    
 
             hrow = size(h)[1] 
             Lh=round(Int,(hrow-1)/2)
             h=h/h[Lh+1] # normalization so that h(0) = 1.0
-
             ti=t[icol]
             taumax=minimum([ti-1,xrow-ti,round(N/2)-1,Lh])
             tau=round(Int64,-taumax:taumax); indices=round(Int64,rem(N+tau,N)+1)
@@ -72,32 +71,17 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
             end
 
             if isnan(f)[1] 
-#                if silent==0 println("Use fft.") end
-                tfrvecp=fft(tfrvec)
+                tfrvecp=fft(tfrvec) #fft
             elseif use_nufft
-#                if silent==0 println("Use nufft.") end
-                tfrvecp=jnufft.call_ionufft1d2(f,tfrvec,-1,10.0^-28)[1:Nf]
+                tfrvecp=jnufft.call_ionufft1d2(f,tfrvec,-1,10.0^-28)[1:Nf] #nufft
             else
-#                if silent==0 println("Use dfft.") end 
                 m=collect(1:N)
                 for j=1:Nf                
-                    tfrvecp[j]=sum(tfrvec.*exp(-2.0*pi*im*(m[:]-1)*(f[j]-1)/N))
+                    tfrvecp[j]=sum(tfrvec.*exp(-2.0*pi*im*(m[:]-1)*(f[j]-1)/N)) #dft
                 end            
             end
             fhs=indmax(abs(tfrvecp))            
-            ##### CRITERION #####
-            sigmaeA=1.0/3.16 # sigma_epsilon/A-> seq
-            kappa=5.0
-            delkappa=0.97
-            #####################
-            sigmahs=sqrt((kappa+delkappa)*sigmaeA^2/(hlength^3))
-
-#            if icol==177 || icol==176
-#                println("---",icol)
-#                println("f ",fhs)
-#                println("sigma ",sigmahs)
-#                println("---")
-#            end
+            sigmahs=sqrt((kappa+delkappa)*sigmaeA[icol]^2/(hlength^3))
 
             if ih>1 && abs(fhsprev - fhs)/(2.0*N) > (kappa+delkappa)*(sigmahsprev+sigmahs)      
                 tfrnew[:,icol]=tfrvecprev
@@ -107,17 +91,14 @@ function awpwv(x,y=NaN,t=NaN,f=NaN,N=NaN,h=NaN,silent=0,method="mean",nwindow=4,
                 tfrnew[:,icol]=tfrvecprev
                 push!(windows,harray[ih-1])
                 crit=false
-            end
-            
+            end            
+
             fhsprev=fhs
             sigmahsprev=sigmahs
             tfrvecprev=copy(tfrvecp)
-        end
-        
+        end        
     end
-    #println(windows)
     return tfrnew, windows
-
 end
 
 end
@@ -128,20 +109,3 @@ end
 #z=DSP.Util.hilbert(y)
 #tfr=cohenclass.tfrpwv(z)
 #tfr,windows=pwvaw.awpwv(z)
-#println(tfr[:,1])
-#println(tfr[:,8])
-
-
-#tfr=cohenclass.tfrwv(z,NaN,NaN,[1,2],NaN,0)
-##tfr=cohenclass.tfrpwv(z)
-#println(tfr)
-
-
-#import DSP
-#nsample=1024
-#x=linspace(0.0,nsample,nsample)
-#y=x
-#z=DSP.Util.hilbert(y)
-#tfr,windows=pwvaw.awpwv(z)
-#println(windows)
-#19
