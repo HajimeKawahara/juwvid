b23781f75f33594d141f6993826a29bc7f0bade4
Hajime Kawahara <divrot@gmail.com>
2016-04-08 16:57:29 +0900

    s-method

--- smethod.jl
+++ smethod.jl
@@ -1,7 +1,10 @@
 module smethod
 using stft 
 
-function tfrsm(x,Lp,f=NaN,nwindow=4)
+function tfrsm(x,Lp,f=NaN,nwindow=4,fps=NaN,fpe=NaN)
+
+    #fps: pick-up frequency (start)
+    #fpe: pick-up frequency (end)
 
     if isnan(f)[1] 
         tfrstft=stft.tfrstft(x,NaN,NaN,NaN,NaN,nwindow)    
@@ -11,13 +14,24 @@ function tfrsm(x,Lp,f=NaN,nwindow=4)
 
     nsamplef=size(tfrstft)[1]
     nsamplet=size(tfrstft)[2]
-    sm=zeros(nsamplef,nsamplet)
 
-    for k=1:nsamplef
-        sm[k,:]=(tfrstft[k,:].*conj(tfrstft[k,:]))
+    if isnan(fps) || isnan(fpe) 
+        ks=1
+        ke=nsamplef 
+        sm=zeros(nsamplef,nsamplet)
+    else
+        ks=maximum([1,2*fps-Lp])
+        ke=minimum([nsamplef,2*fpe+Lp])
+        sm=zeros(ke-ks+1,nsamplet)
+    end
+
+#    for k=1:nsamplef
+    for k=ks:ke
+        kq=k-ks+1
+        sm[kq,:]=(tfrstft[k,:].*conj(tfrstft[k,:]))
         for i=1:Lp
             if k+i<=nsamplef && k-i>0
-                sm[k,:]=sm[k,:]+2*real(tfrstft[k+i,:].*conj(tfrstft[k-i,:]))
+                sm[kq,:]=sm[kq,:]+2*real(tfrstft[k+i,:].*conj(tfrstft[k-i,:]))
             end
         end
     end
